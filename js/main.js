77adbece791ab7c6d4dfc8b557c74e915e841167
Alex100688 <alessiozingaro2@gmail.com>
2022-11-05 15:46:55 +0100

    Aggiunge un ciclo while su 5 numeri random

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
+
+
+  //MAIN//
+  const numbers=document.querySelector(".numbers");
+  const numberCell=100;
+  const listNumber=[];
+  while (listNumber.length < 5) {
+    const numberRandom=getRndInteger(1, numberCell);
+    if (listNumber.includes(numberRandom)===false) {
+        listNumber.push(numberRandom);  
+    }
+  };
+  numbers.append(listNumber);
+  
+   
+   
