529705b4844887e51800fa2f6b0e1d81f7a69e81
Alex100688 <alessiozingaro2@gmail.com>
2022-11-05 16:42:40 +0100

    Aggiunge un setTimeout di 30(s).

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
-
+function game() {
+  numbers.classList.add("gioco");
+}
 
   //MAIN//
-  const numbers=document.querySelector(".numbers");
+  const numbers=document.getElementById("numbers");
   const numberCell=100;
   const listNumber=[];
   while (listNumber.length < 5) {
@@ -17,7 +19,7 @@ function getRndInteger(min, max) {
         listNumber.push(numberRandom);  
     }
   };
-  numbers.append(listNumber);
   
-   
-   
+  numbers.append(listNumber);
+   setTimeout(game, 30000);
+ 
